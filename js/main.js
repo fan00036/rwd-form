c3b8e62d89ff1a4f986508545badceca5ac50e5e
Jinhong Fan <helen@HELENs-MacBook-Pro.local>
2014-11-05 08:51:20 -0500

    12345
    12345

--- js/main.js
+++ js/main.js
@@ -3,5 +3,14 @@ document.addEventListener("DOMContentLoaded", init);
 
 function init(){
 	//startup script
-	
+	 $( "#slider" ).slider({
+      range: "min",
+      value: 1000,
+      min: 100,
+      max: 2000,
+      slide: function( event, ui ) {
+        $( "#price" ).val( "$" + ui.value );
+      }
+    });
+    $( "#price" ).val( "$" + $( "#slider" ).slider( "value" ) );
 }
